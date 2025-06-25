51f5aedb97423b0a7cc546a4148a0d43bbc0db2d
PreeumSaha <2031144@iub.edu.bd>
2025-06-25 20:34:37 +0600

    [UPDATE]: project contribution part

---
+++ footer.php
@@ -0,0 +1,171 @@
+<footer class="footer">
+
+<div class="footer-top">
+  <div class="container">
+
+    <div class="footer-brand">
+
+      <a href="#" class="logo">Nextgen Gamer Nation</a>
+
+      <p class="footer-text">
+        Nextgen Gamer Nation -Explore a wide range of authentic and quality products
+      </p>
+
+      <ul class="contact-list">
+
+        <li class="contact-item">
+          <div class="contact-icon">
+            <ion-icon name="location"></ion-icon>
+          </div>
+
+          <address class="item-text">
+            Address : Dhaka, Bangladesh
+          </address>
+        </li>
+
+        <li class="contact-item">
+          <div class="contact-icon">
+            <ion-icon name="headset"></ion-icon>
+          </div>
+
+          <a href="tel:+8801600000000" class="item-text">Phone : +8801600000000</a>
+        </li>
+
+        <li class="contact-item">
+          <div class="contact-icon">
+            <ion-icon name="mail-open"></ion-icon>
+          </div>
+
+          <a href="mailto:info@ngn.com" class="item-text">Email : info@ngn.com</a>
+        </li>
+
+      </ul>
+
+    </div>
+
+    <ul class="footer-list">
+
+      <li>
+        <p class="footer-list-title">Products</p>
+      </li>
+
+      <li>
+        <a href="#" class="footer-link">Graphics card (10)</a>
+      </li>
+
+      <li>
+        <a href="#" class="footer-link">T-Shirt (6)</a>
+      </li>
+
+      <li>
+        <a href="#" class="footer-link">VR-BOX (9)</a>
+      </li>
+
+      <li>
+        <a href="#" class="footer-link">X-BOX (3)</a>
+      </li>
+
+      <li>
+        <a href="#" class="footer-link">PS5 (3)</a>
+      </li>
+
+    </ul>
+
+    <ul class="footer-list">
+
+      <li>
+        <p class="footer-list-title">Need Help?</p>
+      </li>
+
+      <li>
+        <a href="#" class="footer-link">Terms & Conditions</a>
+      </li>
+
+      <li>
+        <a href="#" class="footer-link">Privacy Policy</a>
+      </li>
+
+      <li>
+        <a href="#" class="footer-link">Refund Policy</a>
+      </li>
+
+      <li>
+        <a href="#" class="footer-link">Affiliate</a>
+      </li>
+
+      <li>
+        <a href="#" class="footer-link">Use Cases</a>
+      </li>
+
+    </ul>
+
+    <div class="footer-wrapper">
+
+      <div class="social-wrapper">
+
+        <p class="footer-list-title">Follow Us</p>
+
+        <ul class="social-list">
+
+          <li>
+            <a href="#" class="social-link" style="background-color: #3b5998">
+              <ion-icon name="logo-facebook"></ion-icon>
+            </a>
+          </li>
+
+          <li>
+            <a href="#" class="social-link" style="background-color: #55acee">
+              <ion-icon name="logo-twitter"></ion-icon>
+            </a>
+          </li>
+
+          <li>
+            <a href="#" class="social-link" style="background-color: #d71e18">
+              <ion-icon name="logo-pinterest"></ion-icon>
+            </a>
+          </li>
+
+          <li>
+            <a href="#" class="social-link" style="background-color: #1565c0">
+              <ion-icon name="logo-linkedin"></ion-icon>
+            </a>
+          </li>
+
+        </ul>
+
+      </div>
+
+      <div class="footer-newsletter">
+
+        <p class="footer-list-title">Newsletter Sign Up</p>
+
+        <form action="" class="footer-newsletter">
+          <input type="email" name="email_address" aria-label="email" placeholder="Enter your email" required
+            class="email-field">
+
+          <button type="submit" class="footer-btn" aria-label="submit">
+            <ion-icon name="rocket"></ion-icon>
+          </button>
+        </form>
+
+      </div>
+
+    </div>
+
+  </div>
+</div>
+
+<div class="footer-bottom">
+  <div class="container">
+
+    <p class="copyright">
+      &copy; 2025 Nextgen Gamer Nation. All Right Reserved<a href="#" class="copyright-link"></a>
+    </p>
+
+    <img src="./assets/images/footer-bottom-img.png" width="340" height="21" loading="lazy" alt=""
+      class="footer-bottom-img">
+
+  </div>
+</div>
+
+</footer>
